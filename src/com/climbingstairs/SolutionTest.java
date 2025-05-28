2568c79e59f5a98035681e2d08768da58303adee
fey-tech <158850884+fey-tech@users.noreply.github.com>
2025-05-28 11:49:36 +0200

    optimized for space, added test cases
    O(1) space

--- src/com/climbingstairs/SolutionTest.java
+++ src/com/climbingstairs/SolutionTest.java
@@ -5,6 +5,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
+    @DisplayName("climb to one")
+    @Test
+    void one() {
+        int actualSteps = Solution.climbStairs(1); //1
+        int expectedSteps = 1;
+        assertEquals(expectedSteps, actualSteps);
+    }
     @DisplayName("climb to two")
     @Test
     void two() {
@@ -12,7 +19,7 @@ public class SolutionTest {
         int expectedSteps = 2;
         assertEquals(expectedSteps, actualSteps);
     }
-    @DisplayName("climb to 3")
+    @DisplayName("climb to three")
     @Test
     void three() {
         int actualSteps = Solution.climbStairs(3); //111, 12, 21
@@ -24,6 +31,13 @@ public class SolutionTest {
     void five() {
         int actualSteps = Solution.climbStairs(5); //11111, 1112, 1121, 1211, 2111, 221, 212, 122
         int expectedSteps = 8;
+        assertEquals(expectedSteps, actualSteps);
+    }
+    @DisplayName("climb to fourty-four")
+    @Test
+    void fourtyFour() {
+        int actualSteps = Solution.climbStairs(44); //I'm not listing every possibility :)
+        int expectedSteps = 1134903170;
         assertEquals(expectedSteps, actualSteps);
     }
 }
